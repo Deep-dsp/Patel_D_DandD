36bc0898a14f9d69576b441d2861aa5af49ca0c6
Patel <d_patel109038@ats-hr420-dm24.fc.ca>
2020-01-31 13:31:55 -0500

    switch images is working

--- script/app.js
+++ script/app.js
@@ -1,13 +1,21 @@
 (() => {
 	// set up the puzzle pieces and boards
 
-	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img')
+				gameBoard = document.querySelector('.puzzle-board'),
+				puzzlePieces = document.querySelectorAll('.puzzle-pieces img');
+
+  const pieceNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
 	function changeImageSet()
 	{
-		//chnage all image elements on the page -> dragabble image sources,
-		//and set the drop zone background
-		debugger;
+		//chnage all image elements on the page -> dragabble image sources
+
+		//Change image on left box
+		pieceNames.forEach((piece, index) => puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg` );
+		//and set the drop zone background image based on the puzzle that user selects
+		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleref}.jpg)`;
+		//debugger;
 	}
 
 	//add event handler -> how is user going to use our app
@@ -15,4 +23,8 @@
 
 	//click on the buttom buttons to change the puzzle image we are working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+	//call the function nad pass in the first nav button as a reference
+	// Call, apply and Bind --> 
+	changeImageSet.call(puzzleButtons[0]);
 })();
