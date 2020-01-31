eae656fc92e961a40b38cd2c77d0b265a127fa29
Patel <d_patel109038@ats-hr420-dm24.fc.ca>
2020-01-31 14:41:03 -0500

    piecesDragDrop

--- script/app.js
+++ script/app.js
@@ -3,7 +3,8 @@
 
 	const puzzleButtons = document.querySelectorAll('#buttonHolder img')
 				gameBoard = document.querySelector('.puzzle-board'),
-				puzzlePieces = document.querySelectorAll('.puzzle-pieces img');
+				puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
+				dropZones = document.querySelectorAll('.drop-zone');
 
   const pieceNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
@@ -12,19 +13,50 @@
 		//chnage all image elements on the page -> dragabble image sources
 
 		//Change image on left box
-		pieceNames.forEach((piece, index) => puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg` );
+		pieceNames.forEach((piece, index) => {
+																						puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`;
+																						puzzlePieces[index].id = `${piece + this.dataset.puzzleref}`;
+																					});
 		//and set the drop zone background image based on the puzzle that user selects
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleref}.jpg)`;
 		//debugger;
 	}
 
+	function allowDrag(event)
+	{
+		console.log('Started Dragging an Image');
+
+		event.dataTransfer.setData("text/plain", this.id);
+		// debugger;
+	}
+
+	function allowDragOver(event)
+	{
+		event.preventDefault();
+		console.log('Dragged Over Drop Zone');
+	}
+
+	function allowDrop(event)
+	{
+		//event.preventDefault();
+		console.log('Dropped on a Drop Zone');
+
+		let currentImage = event.dataTransfer.getData('text/plain');
+		event.target.appendChild(document.querySelector(`#${currentImage}`));
+	}
 	//add event handler -> how is user going to use our app
 	//what triggers do we need?
 
 	//click on the buttom buttons to change the puzzle image we are working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
 
+	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
+
+	dropZones.forEach(zone => { zone.addEventListener(('dragover'), allowDragOver);
+														 zone.addEventListener(('drop'), allowDrop);
+													 });
+
 	//call the function nad pass in the first nav button as a reference
-	// Call, apply and Bind --> 
+	// Call, apply and Bind -->
 	changeImageSet.call(puzzleButtons[0]);
 })();
