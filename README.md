4a31d7cb45ed148e68de761afbe5ff9203fc7a98
Patel <d_patel109038@ats-kml11-dm03.fc.ca>
2020-03-06 20:27:24 -0500

    crushingBugs

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # My Awesome Drag And Drop Project
+
+This is the javascript game project which is developed to learn the fundamentals of the javascript and how to make each operations interactive.
+
+## Prerequisites
+
+First of all, it is better to have clear concept about the game and what kind of operations needs to be performed before jumping in to the developing part.
+
+## Authors
+
+1. Deepkumar Patel (0953510)
+
+## Lisence 
+
+This project is lisenced under **MIT**.
+© Copyrights Reserved.
--- script/app.js
+++ script/app.js
@@ -4,7 +4,8 @@
 	const puzzleButtons = document.querySelectorAll('#buttonHolder img')
 				gameBoard = document.querySelector('.puzzle-board'),
 				puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
-				dropZones = document.querySelectorAll('.drop-zone');
+				dropZones = document.querySelectorAll('.drop-zone'),
+				pieceContainer = document.querySelector('.puzzle-pieces');
 
   const pieceNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
@@ -14,8 +15,8 @@
 
 		//Change image on left box
 		pieceNames.forEach((piece, index) => {
-																						puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`;
-																						puzzlePieces[index].id = `${piece + this.dataset.puzzleref}`;
+		puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`;
+		puzzlePieces[index].id = `${piece + this.dataset.puzzleref}`;
 																					});
 		//and set the drop zone background image based on the puzzle that user selects
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleref}.jpg)`;
@@ -32,8 +33,12 @@
 
 	function allowDragOver(event)
 	{
-		event.preventDefault();
+		if(this.childNodes.length == 0)
+		{
+			event.preventDefault();
+		}
 		console.log('Dragged Over Drop Zone');
+
 	}
 
 	function allowDrop(event)
@@ -44,11 +49,21 @@
 		let currentImage = event.dataTransfer.getData('text/plain');
 		event.target.appendChild(document.querySelector(`#${currentImage}`));
 	}
+
+	function resetPuzzlePiecesCon()
+	{
+		for(let j=0;j<puzzlePieces.length;j=j+1)
+		{
+			pieceContainer.appendChild(puzzlePieces[j]);
+		}
+	}
 	//add event handler -> how is user going to use our app
 	//what triggers do we need?
 
 	//click on the buttom buttons to change the puzzle image we are working with
-	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+	puzzleButtons.forEach(button => { button.addEventListener('click', changeImageSet),
+									  button.addEventListener('click', resetPuzzlePiecesCon)
+									});
 
 	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
 
